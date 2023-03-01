053e664b7abe4fcbd339b22770709310c5362926
Plague Fox <plugfox@gmail.com>
2023-03-01 14:35:39 +0400

    Add screenshots in

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io' as io;
+
 import 'package:app/src/common/initialization/initialization.dart';
 import 'package:app/src/common/widget/app.dart';
 import 'package:app/src/feature/logs/widget/logs_screen.dart';
@@ -5,6 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'src/util/async_finder.dart';
+import 'src/util/screenshot.dart';
 
 void main() {
   // ignore: unused_local_variable
@@ -20,6 +23,10 @@ void main() {
     });
 
     testWidgets('App', (tester) async {
+      // Prepare for screenshot.
+      final shot = screenshot(tester);
+      io.Directory('integration_test/screenshots').createSync(recursive: true);
+
       // Build our app and trigger a frame.
       await tester.pumpWidget(const App());
       await asyncFinder(tester: tester, finder: () => find.byType(App));
@@ -27,7 +34,10 @@ void main() {
       // Check initial state.
       expect(find.byType(App), findsOneWidget);
       expect(find.byType(LogsScreen), findsOneWidget);
-      expect(find.text('Shop'), findsAtLeastNWidgets(1));
+      expect(find.text('Events'), findsAtLeastNWidgets(1));
+
+      final appScreenshot = await shot();
+      io.File('integration_test/screenshots/app.png').writeAsBytesSync(appScreenshot);
     });
   });
 }
---
+++ integration_test/src/util/screenshot.dart
@@ -0,0 +1,41 @@
+import 'dart:io' as io;
+import 'dart:typed_data' as td;
+import 'dart:ui' as ui;
+
+import 'package:flutter/rendering.dart' show RenderRepaintBoundary;
+import 'package:flutter/widgets.dart' show WidgetsApp;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+/// The [_$pixelRatio] describes the scale between the logical pixels and the
+/// size of the output image. It is independent of the
+/// [dart:ui.FlutterView.devicePixelRatio] for the device, so specifying 1.0
+/// (the default) will give you a 1:1 mapping between logical pixels and the
+/// output pixels in the image.
+const double _$pixelRatio = 1.0;
+
+/// Returns a function that takes a screenshot of the current state of the app.
+Future<List<int>> Function([String? name]) screenshot(
+  WidgetTester tester, [
+  IntegrationTestWidgetsFlutterBinding? binding,
+]) =>
+    ([String? name]) async {
+      await tester.pump();
+      if (binding != null && name != null && (io.Platform.isAndroid || io.Platform.isIOS)) {
+        if (io.Platform.isAndroid) await binding.convertFlutterSurfaceToImage();
+        return await binding.takeScreenshot(name);
+      } else {
+        final element = tester.firstElement(find.byType(WidgetsApp));
+        RenderRepaintBoundary? boundary;
+        element.visitAncestorElements((element) {
+          final renderObject = element.renderObject;
+          if (renderObject is RenderRepaintBoundary) boundary = renderObject;
+          return true;
+        });
+        if (boundary == null) throw StateError('No RenderRepaintBoundary found');
+        final image = await boundary!.toImage(pixelRatio: _$pixelRatio);
+        final bytes = await image.toByteData(format: ui.ImageByteFormat.png);
+        if (bytes is! td.ByteData) throw StateError('Error converting image to bytes');
+        return bytes.buffer.asUint8List();
+      }
+    };
